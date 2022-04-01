9a8c609b75c89a30ae1da1bfae3cce08ad5663a4
morgan <morgan@dunamu.com>
2022-04-01 17:47:50 +0900

    polish dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,11 +25,13 @@ repositories {
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core-jvm:1.6.0")
 
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("com.h2database:h2:2.1.210")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("io.kotest:kotest-runner-junit5:5.2.2")
     testImplementation("io.kotest.extensions:kotest-extensions-spring:1.1.0")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.6.0")
 }
 
 tasks.withType<KotlinCompile> {
